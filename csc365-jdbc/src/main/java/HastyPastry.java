35ceefe298989a522dc941683fd6e611b96ad345
mcalanog <59623992+mcalanog@users.noreply.github.com>
2021-06-09 15:32:08 -0700

    Update HastyPastry.java

--- csc365-jdbc/src/main/java/HastyPastry.java
+++ csc365-jdbc/src/main/java/HastyPastry.java
@@ -18,15 +18,29 @@ import java.util.ArrayList;
 public class HastyPastry {
 
     private final String JDBC_URL = "jdbc:h2:~/csc365_lab7";
-    private final String JDBC_USER = "";
-    private final String JDBC_PASSWORD = "";
+    private final String JDBC_USER = "mcalanog";
+    private final String JDBC_PASSWORD = "Spr2021-365-014628155";
     
     public static void main(String[] args) {
 	try {
 	    HastyPastry hp = new HastyPastry();
             hp.initDb();
         hp.createTables();
-	    hp.demo3();
+	    String option = hp.demo3();
+	    while (!input.equals("x")){
+				if (input.equals("1")) {
+					ir.FR1();
+				} else if (input.equals("2")){
+					ir.FR2();
+				} else if (input.equals("3")){
+					ir.FR3();
+				} else if (input.equals("4")){
+					ir.FR4();
+				} else if (input.equals("5")){
+					ir.FR5();
+				}
+				input = ir.demo3();
+			}
 	} catch (SQLException e) {
 	    System.err.println("SQLException: " + e.getMessage());
 	}
@@ -68,19 +82,9 @@ public class HastyPastry {
 		}
 	}
 
-
+	//where is this called?
     // Demo1 - Establish JDBC connection, execute DDL statement
     private void demo1() throws SQLException {
-
-	// Step 0: Load JDBC Driver
-	// No longer required as of JDBC 2.0  / Java 6
-	try{
-	    Class.forName("org.h2.Driver");
-	    System.out.println("H2 JDBC Driver loaded");
-	} catch (ClassNotFoundException ex) {
-	    System.err.println("Unable to load JDBC Driver");
-	    System.exit(-1);
-	}
 
 	// Step 1: Establish connection to RDBMS
 	try (Connection conn = DriverManager.getConnection(JDBC_URL,
@@ -106,37 +110,6 @@ public class HastyPastry {
     }
     
 
-    // Demo2 - Establish JDBC connection, execute SELECT query, read & print result
-    private void demo2() throws SQLException {
-
-	// Step 1: Establish connection to RDBMS
-	try (Connection conn = DriverManager.getConnection(JDBC_URL,
-							   JDBC_USER,
-							   JDBC_PASSWORD)) {
-	    // Step 2: Construct SQL statement
-	    String sql = "SELECT * FROM hp_goods";
-
-	    // Step 3: (omitted in this example) Start transaction
-
-	    // Step 4: Send SQL statement to DBMS
-	    try (Statement stmt = conn.createStatement();
-		 ResultSet rs = stmt.executeQuery(sql)) {
-
-		// Step 5: Receive results
-		while (rs.next()) {
-		    String flavor = rs.getString("Flavor");
-		    String food = rs.getString("Food");
-		    float price = rs.getFloat("Price");
-                    java.sql.Date availUntil = rs.getDate("AvailUntil");
-		    System.out.format("%s %s ($%.2f) %s %n", flavor, food, price, availUntil);
-		}
-	    }
-
-	    // Step 6: (omitted in this example) Commit or rollback transaction
-	}
-	// Step 7: Close connection (handled by try-with-resources syntax)
-    }
-
 
     // Demo3 - Establish JDBC connection, execute DML query (UPDATE)
     // -------------------------------------------
@@ -144,7 +117,7 @@ public class HastyPastry {
     // -------------------------------------------
     private void demo3() throws SQLException {
 
-        demo2(); // print contents of goods table
+        //demo2(); // print contents of goods table
         
 	// Step 1: Establish connection to RDBMS
 	try (Connection conn = DriverManager.getConnection(JDBC_URL,
@@ -152,36 +125,76 @@ public class HastyPastry {
 							   JDBC_PASSWORD)) {
 	    // Step 2: Construct SQL statement
 	    Scanner scanner = new Scanner(System.in);
-	    System.out.print("\n Enter a flavor: ");
-	    String flavor = scanner.nextLine();
-	    System.out.format("\n Until what date will %s be available (YYYY-MM-DD)? ", flavor);
-	    String availUntilDate = scanner.nextLine();
+	    //added:
+	    System.out.print("\n Command options: \n");
+		System.out.print("\n View Rooms and Rates: (1)");
+		System.out.print("\n Make Reservation: (2)");
+		System.out.print("\n Make Change to a Current Reservation: (3)");
+		System.out.print("\n Cancel Reservation: (4)");
+		System.out.print("\n Show Revenue Summaries (5)");
+		System.out.println("\n I am done! : (x)");
 
-	    // -------------------------------------------
-	    // Never (ever) write database code like this!
-	    // -------------------------------------------
-	    String updateSql = "UPDATE hp_goods SET AvailUntil = '" + availUntilDate + "' " +
-		               "WHERE Flavor = '" + flavor + "'";
-
-	    // Step 3: (omitted in this example) Start transaction
-	    
-	    try (Statement stmt = conn.createStatement()) {
-		
-		// Step 4: Send SQL statement to DBMS
-		int rowCount = stmt.executeUpdate(updateSql);
-		
-		// Step 5: Handle results
-		System.out.format("Updated %d records for %s pastries%n", rowCount, flavor);		
-	    }
-
-	    // Step 6: (omitted in this example) Commit or rollback transaction
+		System.out.print("\n Enter selection here: ");
+		//changed to roomcode
+	    String option = scanner.nextLine();
+	    return option;
 	    
 	}
 	// Step 7: Close connection (handled implcitly by try-with-resources syntax)
-
-        demo2();  // print contents of goods table
         
     }
+    private void FR1() throws SQLException {
+
+		// Step 1: Establish connection to RDBMS
+		try (Connection conn = DriverManager.getConnection(JDBC_URL,
+				JDBC_USER,
+				JDBC_PASSWORD)) {
+
+			java.sql.Date sqlDate = new java.sql.Date(System.currentTimeMillis());
+			//NotAvailNow as Occupied
+			//AvailNow as Vacant
+			String fr1 =
+					"with Occupied as (\n" +
+							"    select RoomName, CheckOut as NextAvail\n" +
+							"    from lab7_rooms as rooms, lab7_reservations as reservations\n" +
+							"	 where rooms.RoomId = reservations.Room \n" +
+							"    and CheckIn <= CURRENT_DATE\n" +
+							"        and Checkout >= CURRENT_DATE\n" +
+							"),\n" +
+							"Vacant as (\n" +
+							"    select RoomName, CURRENT_DATE AS NextAvail\n" +
+							"    from lab7_rooms as rooms \n" +
+							"    where RoomName NOT IN (select RoomName from Occupied)\n" +
+							"    group by RoomName \n" +
+							"),\n" +
+							"NextReservation as (\n" +
+							"    select RoomName, min(CheckIn) as NextCheckIn\n" +
+							"    from lab7_rooms as rooms, lab7_reservations as reservations\n" +
+							"	 where rooms.RoomId = reservations.Room\n" +
+							"    and CheckIn > CURRENT_DATE\n" +
+							"    group by RoomName)\n" +
+							"select avail.RoomName, NextAvail, NextCheckIn from (select *\n" +
+							"FROM Vacant\n" +
+							"UNION \n" +
+							"select * \n" +
+							"FROM Occupied) as available, NextReservation\n"+
+							"where NextReservation.RoomName = available.RoomName";
+
+			try (PreparedStatement stmt = conn.prepareStatement(fr1);) {
+				ResultSet rs = stmt.executeQuery();
+
+				while (rs.next()) {
+					String name = rs.getString("RoomName");
+					java.sql.Date nextAvail = rs.getDate("NextAvail");
+					java.sql.Date nextcheck = rs.getDate("NextCheckIn");
+					System.out.format("%s %s %s %n", name, nextAvail, nextcheck);
+				}
+			}catch(Exception ex){
+				ex.printStackTrace();
+			}
+		}
+	}
+
 
 
     // Demo4 - Establish JDBC connection, execute DML query (UPDATE) using PreparedStatement / transaction    
@@ -286,4 +299,3 @@ public class HastyPastry {
     
 
 }
-
